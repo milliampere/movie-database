d90bac71a10037cf5ff62a441fbc3e4eae4ae78d
milliampere <millla@hotmail.com>
2017-02-28 23:42:50 +0100

    finished code

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@
  */
 document.addEventListener('DOMContentLoaded', function(event) {
 	document.getElementById('addMovieButton').addEventListener('click', MovieDatabase.addMovie);
+	document.getElementById('editMovieButton').addEventListener('click', MovieDatabase.editMovie);
 	document.getElementById('sortByGenresButton').addEventListener('click', MovieDatabase.sortByGenre);
 	document.getElementById('resetButton').addEventListener('click', () => {MovieDatabase.appendMovies();});
   document.getElementById('yearSortSelect').addEventListener('change', MovieDatabase.sortByYear);
+  document.getElementById('sortByRatingsButton').addEventListener('click', MovieDatabase.sortByRatings);
   
   MovieDatabase.appendGenresList('genresSortList');
 	MovieDatabase.appendGenresList('genresAddList');
@@ -13,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
 	MovieDatabase.fillSelectWithYears(document.getElementById('year'));
 
   MovieDatabase.appendTopLists(); 
-  MovieDatabase.appendMovies(); //Fill index with movies
+  MovieDatabase.appendMovies(); 	//Fill index with movies
 });
 
 /**
@@ -32,7 +34,7 @@ const MovieDatabase  = (function(){
 	    title: 'The Prestige',
 	    year: 2006,
 	    genres: ['Drama', 'Mystery', 'Sci-Fi'],
-	    ratings: [4, 5, 5, 5], 
+	    ratings: [4, 8, 7, 10], 
 	    description: 'Two stage magicians engage in competitive one-upmanship in an attempt to create the ultimate stage illusion.',
 	    image: "http://www.hidefninja.com/community/attachments/movie-night-clipart-9cp4q9xce-jpeg.279121/"
 		},
@@ -40,23 +42,37 @@ const MovieDatabase  = (function(){
 	    title: 'Her',
 	    year: 2013,
 	    genres: ['Drama', 'Romance', 'Sci-Fi'],
-	    ratings: [5, 4, 3, 4], 
+	    ratings: [5, 7, 3, 4], 
 	    description: 'A lonely writer develops an unlikely relationship with an operating system designed to meet his every need.',
 	    image: "http://www.hidefninja.com/community/attachments/movie-night-clipart-9cp4q9xce-jpeg.279121/"
-		},
-		{
-	    title: 'Inception',
-	    year: 2010,
-	    genres: ['Action', 'Adventure', 'Sci-Fi'],
-	    ratings: [3, 4, 5, 4],
-	    description: 'A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.',
 		},
 		{
 	    title: 'Interstellar',
 	    year: 2014,
 	    genres: ['Adventure', 'Drama', 'Sci-Fi'],
-	    ratings: [3, 4, 3, 2],
+	    ratings: [3, 4, 2],
 	    description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+		},
+		{
+	    title: 'Inception',
+	    year: 2010,
+	    genres: ['Action', 'Adventure', 'Sci-Fi'],
+	    ratings: [3, 4, 5, 6],
+	    description: 'A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.',
+		},
+		{
+	    title: 'Shutter Island',
+	    year: 2010,
+	    genres: ['Mystery', 'Thriller'],
+	    ratings: [7, 6, 8],
+	    description: 'In 1954, a U.S. marshal investigates the disappearance of a murderess who escaped from a hospital for the criminally insane.',
+		},
+		{
+	    title: 'Logan: The Wolverine',
+	    year: 2017,
+	    genres: ['Action', 'Drama', 'Sci-Fi'],
+	    ratings: [6, 6, 8],
+	    description: 'In the near future, a weary Logan cares for an ailing Professor X in a hide out on the Mexican border. A young mutant arrives, being pursued by dark forces.',
 		},
 	];
 
@@ -66,7 +82,7 @@ const MovieDatabase  = (function(){
 		'Comedy',
 		'Drama',
 		'Mystery',
-		'Romance',
+		'Romance', 
 		'Sci-Fi',
 		'Thriller',
 	];
@@ -101,6 +117,18 @@ const MovieDatabase  = (function(){
     	});
     }, 
 
+    /**
+     *  Get movies with ratings between two numbers
+     * @param  {Number} to   
+     * @param  {Number} from 
+     * @return {Array}    				Array of movies with specific ratings  
+     */
+    getMoviesThisRatings: function(from, to){
+    	return movies.filter((movie) => {
+    		return (MovieDatabase.getRating(movie) > from && MovieDatabase.getRating(movie) <= to);
+    	});
+    },
+
 		/**
 		 * Get movies by movie genres
 		 * @param  {Array} genres  	Array of chosen movie genres
@@ -125,10 +153,13 @@ const MovieDatabase  = (function(){
   	 * @return {Number}					The average rating  
   	 */
 	 	getRating: function(movie){
-			var sumOfRatings = movie.ratings.reduce((total, rating) => {
+	 		if (movie.ratings.length === 0) {
+	 			return '-';
+	 		}
+			let sumOfRatings = movie.ratings.reduce((total, rating) => {
 				return total + rating;
 			}, 0);
-			var numberOfRatings = movie.ratings.length;
+			let numberOfRatings = movie.ratings.length;
 			return (sumOfRatings/numberOfRatings).toFixed(1);
 		},
 
@@ -138,7 +169,7 @@ const MovieDatabase  = (function(){
      */
     getTopRatedMovie: function(){
     	return movies.reduce((prev, curr) => {
-    		if(this.getRating(prev) < this.getRating(curr)){
+    		if(parseInt(this.getRating(prev)) < parseInt(this.getRating(curr))){
     			return curr;
     		}
     		else {
@@ -153,7 +184,7 @@ const MovieDatabase  = (function(){
      */
 		getWorstRatedMovie: function(){
     	return movies.reduce((prev, curr) => {
-    		if(this.getRating(prev) > this.getRating(curr)){
+    		if(parseInt(this.getRating(prev)) > parseInt(this.getRating(curr))){
     			return curr;
     		}
     		else {
@@ -169,16 +200,15 @@ const MovieDatabase  = (function(){
 		 */
 		rateMovie: function(movie, rating){
 			movie.ratings.push(parseInt(rating));
-			console.log('hej');
 		},
 
 		/**
-		 * Create a list of all genres with checkboxes in HTML
+		 * Create a list of all genres with checkboxes in index.html
 		 * @param {String} id 			Id of the element to append the list to
 		 */
 		appendGenresList: function(id){
-			var element = document.getElementById(id);
-			var htmlChunk = '';
+			let element = document.getElementById(id);
+			let htmlChunk = '';
 			for(let genre of genres){
 				htmlChunk += `
 					<label class="checkbox-inline"><input type="checkbox" id="${genre}" value="${genre}">${genre}</label>`;
@@ -188,56 +218,52 @@ const MovieDatabase  = (function(){
 
 		/**
 		 * Add click events to movie list
+		 * (This is very messy code)
 		 */
 		addClickEventsToMovies: function(){
-			const ratingSelect = document.getElementsByClassName('ratingSelect');
-			const edit = document.getElementsByClassName('edit');
 			let title = document.getElementById('title');
 			let year = document.getElementById('year');
+			let description = document.getElementById('description');
+			let genresAddList = document.getElementById('genresAddList');
 
-			var allMovies = document.getElementsByClassName('movie');
-			var rateButton = document.getElementsByClassName('rateButton');
+			let appendedMovies = document.getElementsByClassName('movie');
+			let rateButton = document.getElementsByClassName('rateButton');
+			let edit = document.getElementsByClassName('edit');
 
 
-			for(let i = 0; i < rateButton.length; i++) {
+			for(let i = 0; i < appendedMovies.length; i++) {
 				rateButton[i].addEventListener('click', function(e){
-					console.log(e.target.previousSibling);
 					for(var movie of movies){
-						if (movie.title == e.target.parentElement.parentElement.parentElement.dataset.title){
-							console.log('hej');
-							//MovieDatabase.rateMovie(movie, this.value);
+						let datasetTitle = e.target.parentElement.parentElement.parentElement.dataset.title;
+						if (movie.title == datasetTitle){
+							let pickedRating = parseInt(e.target.previousSibling.previousSibling.value);
+							MovieDatabase.rateMovie(movie, pickedRating);			// Add rating to movie array
+							MovieDatabase.appendMovies([movie]);			// Load movie with new rating
+							MovieDatabase.appendTopLists();		// Load top list 
 						}
 					}
+				});
+				edit[i].addEventListener("click", function(e) {
+					datasetTitle = e.target.parentElement.parentElement.parentElement.parentElement.dataset.title;
+					for(var movie of movies){
+						if (movie.title == datasetTitle){
+							title.value = movie.title;
+							year.value = movie.year;
+							description.value = movie.description;
 
+							var allInput = genresAddList.querySelectorAll('input');
+
+							for (let j = 0; j < allInput.length; j++) {
+								for(let genre of movie.genres){
+									if (allInput[j].id == genre){
+									 	allInput[j].checked = true;
+									 }
+								}
+							}
+						}
+					}
 				});			
 			}
-
-
-			// for(let i = 0; i < ratingSelect.length; i++) { 
-			// 	ratingSelect[i].addEventListener("change", function() {
-					
-			// 		for(var movie of movies){
-			// 			if (movie.title == this.parentElement.dataset.title){
-			// 				MovieDatabase.rateMovie(movie, this.value);
-			// 			}
-			// 		}
-
-			// 	});
-			// }
-
-			// for(let i = 0; i < edit.length; i++) { 
-			// 	edit[i].addEventListener("click", function() {
-			// 		for(var movie of movies){
-			// 			if (movie.title == this.parentElement.dataset.title){
-			// 				title.value = movie.title;
-			// 				year.value = movie.year;	
-
-			// 			}
-			// 		}
-			// 	});
-			// }
-
-
 		},
 
     /**
@@ -245,7 +271,7 @@ const MovieDatabase  = (function(){
      * @param  {Array} moviesArray 		Array of movie objects
      */
 		appendMovies: function(moviesArray) {
-			const movieList = document.getElementById('movieList');
+			let movieList = document.getElementById('movieList');
 			let htmlChunk = '';
 
 			if(moviesArray===undefined){
@@ -262,7 +288,7 @@ const MovieDatabase  = (function(){
 			
 				for(let movie of moviesArray){
 					
-					var rating = this.getRating(movie);
+					let rating = this.getRating(movie);
 					
 					let genreList = '';
 					for(let genre of movie.genres){
@@ -278,22 +304,20 @@ const MovieDatabase  = (function(){
 														</div>
 														<div class="panel-body">
 															<p>${movie.description}</p>
-															<p>Rating: ${rating} <span class="glyphicon glyphicon-star"></span></p>
+															Rating: ${rating} <span class="glyphicon glyphicon-star"></span>
 															<select id="ratingSelect" class="ratingSelect custom-select mb-2 mr-sm-2 mb-sm-0">
-																<option value="rate">-</option>
-																<option value="1">1</option>
-																<option value="2">2</option>
-																<option value="3">3</option>
-																<option value="4">4</option>
-																<option value="5">5</option>
-																<option value="6">6</option>
-																<option value="7">7</option>
-																<option value="8">8</option>
-																<option value="9">9</option>
 																<option value="10">10</option>
+																<option value="9">9</option>
+																<option value="8">8</option>
+																<option value="7">7</option>
+																<option value="6">6</option>
+																<option value="5">5</option>
+																<option value="4">4</option>
+																<option value="3">3</option>
+																<option value="2">2</option>
+																<option value="1">1</option>
 												    	</select>
-												    	<button type="button" class="btn btn-default rateButton" id="rateButton">Rate</button>
-															<h5>Genres</h5>
+												    	<button type="button" class="btn btn-secondary btn-sm rateButton">Rate</button>
 						    							<ul class="list-group"> ${genreList} </ul>
 						    							<p class="edit"><a href="#">Edit this movie</a></p>
 														</div>
@@ -311,7 +335,7 @@ const MovieDatabase  = (function(){
 		 */
 		getCheckedElements: function(select) {
 		    let result = [];
-		    const checkboxes = select.querySelectorAll("input[type='checkbox']");
+		    let checkboxes = select.querySelectorAll("input[type='checkbox']");
 		    for (let i = 0; i < checkboxes.length; i++) {
 		        if (checkboxes[i].checked){
 		        	result.push(checkboxes[i].id);
@@ -328,7 +352,7 @@ const MovieDatabase  = (function(){
 			let htmlChunk = '<option value="all">All</option>';
 			let y = new Date().getFullYear();
 
-			for(let i = 1990; i <= y; i++){
+			for(let i = 2000; i <= y; i++){
 				htmlChunk += `<option value="${i}">${i}</option>`;
 			}
 			select.innerHTML = htmlChunk;
@@ -341,8 +365,8 @@ const MovieDatabase  = (function(){
 		appendTopLists: function(){
 
 			//Get div-elements
-			const highestRatingList = document.getElementById('highestRatingList');
-			const lowestRatingList = document.getElementById('lowestRatingList');
+			let highestRatingList = document.getElementById('highestRatingList');
+			let lowestRatingList = document.getElementById('lowestRatingList');
 
 			let highestRatingHtmlChunk = '';
 			let lowestRatingHtmlChunk = '';
@@ -366,6 +390,19 @@ const MovieDatabase  = (function(){
 			MovieDatabase.appendMovies(movies);
 		},
 
+		/**
+		 * Sort movies by ratings
+		 */
+		sortByRatings: function(){
+			var fromRating = parseInt(document.getElementById('fromRatingSortSelect').value);
+			var toRating = parseInt(document.getElementById('toRatingSortSelect').value);
+			var moviesThisRatings = MovieDatabase.getMoviesThisRatings(fromRating, toRating);
+			MovieDatabase.appendMovies(moviesThisRatings);
+		},
+
+		/**
+		 * Sort movies by year
+		 */
 		sortByYear: function(){
 			if(this.value === 'all'){
 				MovieDatabase.appendMovies();
@@ -380,43 +417,68 @@ const MovieDatabase  = (function(){
 		 * Add new movie to the movie array 
 		 */
 		addMovie: function(){
-			let title = document.getElementById('title');
-			let year = parseInt(document.getElementById('year'));	
-			let description = document.getElementById('description');			
-			let select = document.getElementById('genresAddList');
+			var title = document.getElementById('title');
+			var year = document.getElementById('year');	
+			var description = document.getElementById('description');			
+			var checked = document.getElementById('genresAddList');
 
 			// Get all checked genres
-    	let genres = MovieDatabase.getCheckedElements(select);
+    	var genres = MovieDatabase.getCheckedElements(checked);
 
 			// Check if input values are valid
 			if(title.value === ""){
 				alert("Well... movie needs a title.");
 			}
+			else if(year.value === 'all'){
+				alert("Pick a year.");
+			}
 			else {
 				const newMovie = new Movie(title.value, year.value, genres, [], description.value);
 				movies.push(newMovie);
-
-				MovieDatabase.appendMovies(); 	//Load the new movie list
-				title.value = '';								//reset input value
-				description.value = '';					//reset input value
+				MovieDatabase.appendMovies([movies[movies.length-1]]); 	//Load the new movie list
+				MovieDatabase.resetInputs();
 			}
 		}, 
 
 		/**
 		 * Edit movie in the movies array 
 		 */
-		editMovie: function(movie){
-			var title = document.getElementById('title');
-			var year = parseInt(document.getElementById('year'));	
+		editMovie: function(){
+			let title = document.getElementById('title');
+			let checked = document.getElementById('genresAddList');
+			let year = document.getElementById('year');
 
-			var editedMovie = new Movie(title, year, ['Drama', 'Comedy'], [5], 'Description of the movie');
-			MovieDatabase.addMovieToArray(newMovie);
-			
+			if(year.value === 'all'){
+				alert("Pick a year.");
+			}
+			else{
+				for(let movie of movies){
+					if (movie.title == title.value){
+						movie.year = year.value;
+						movie.genres = MovieDatabase.getCheckedElements(checked);
+						movie.description = description.value;
+						MovieDatabase.appendMovies([movie]);
+						MovieDatabase.resetInputs();
+					}
+				}		
+			}
 		}, 
 
+		/**
+		 * Reset input values in add/edit movie
+		 */
+		resetInputs: function(){
+			title.value = '';								//reset input value
+			description.value = '';					//reset input value
 
+			var checked = document.getElementById('genresAddList');
+			var allInput = genresAddList.querySelectorAll('input');
 
-
+			// reset checkboxes
+			for (let i = 0; i < allInput.length; i++) {
+				allInput[i].checked = false;
+			}	
+		},
 
 
 	// end of return
@@ -424,5 +486,4 @@ const MovieDatabase  = (function(){
 
 // end of MovieDatabase
 })();
-
 
